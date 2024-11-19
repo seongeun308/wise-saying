f2fe5b1df092c3c6602fc6e847b285d7644efa91
seongeun308 <kse011010@gmail.com>
2024-11-19 15:25:18 +0900

    feat: 종료 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        System.out.println("== 명언 앱 ==");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("명령) ");
+        String command = scanner.next();
+        if (command.equals("종료")) System.exit(0);
     }
 }
